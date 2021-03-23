b2480832a1099195c95ba133b6837681e003ecf9
Théo Macron <akctarus@MacBook-Pro-de-Theo.local>
2021-03-23 16:21:03 +0100

    fix

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const { json } = require('express');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 9090;
-const winston = require('winston');
 
+const PORT = process.env.PORT || 9090;
+
+const winston = require('winston');
+ 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      //
-      // - Write all logs with level `error` and below to `error.log`
-      // - Write all logs with level `info` and below to `combined.log`
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
-    ],
-  });
-app.get('/', function (req, res) {
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level error and below to error.log
+    // - Write all logs with level info and below to combined.log
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+app.get('/', function(req,res) {
+
+    logger.log({
+        level: 'error',
+        message: 'Hello distributed log files!'
+    }); 
+
     return res.status(200).json({
         hello: 'world'
-    });
-});
-logger.log({
-    level: 'error',
-    message: 'Hello distributed log files!'
-});
+    })
+})
 
 app.listen(PORT, () => {
-    console.log('Server listening on port ${PORT}')
-});
-
-
+    console.log('Server listening on ${PORT}')
+})
